d3a66206cb4ed18613577b56208504a41732406d
YouXianMing <YouXianMing1987@126.com>
2016-09-05 22:36:44 +0800

    Add UIView+ScreensShot.swift

---
+++ Swift-Animations/UIView+ScreensShot.swift
@@ -0,0 +1,32 @@
+//
+//  UIView+ScreensShot.swift
+//  Swift-Animations
+//
+//  Created by YouXianMing on 16/9/5.
+//  Copyright © 2016年 YouXianMing. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+    /**
+     Get the view's screen shot, this function may be called from any thread of your app.
+     
+     - returns: The screen shot's image.
+     */
+    func screenShot() -> UIImage? {
+        
+        guard frame.size.height > 0 && frame.size.width > 0 else {
+        
+            return nil
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(frame.size, false, 0)
+        layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+}
