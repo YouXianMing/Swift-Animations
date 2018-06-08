c33e3be62a4b13435194ce7eacda97e718501f6f
YouXianMing <YouXianMing1987@126.com>
2018-06-08 10:18:00 +0800

    Add app's ad.

--- Swift-Animations/AppDelegate.swift
+++ Swift-Animations/AppDelegate.swift
@@ -25,6 +25,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          http://www.cnblogs.com/YouXianMing/
          https://github.com/YouXianMing/YoCelsius
          https://github.com/YouXianMing/Animations
+         
+         独立开发作品 (感兴趣的朋友支持一下^_^) : https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=1390862464&mt=8
          */
 
         window = UIWindow(frame: UIScreen.main.bounds)
